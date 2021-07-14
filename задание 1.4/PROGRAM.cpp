4298cc4e844a9d8150bac8561cd8ea9034ac56ae
UlyanaVenslav <87064872+UlyanaVenslav@users.noreply.github.com>
2021-07-14 13:20:23 +0500

    Update PROGRAM.cpp

--- задание 1.4/PROGRAM.cpp
+++ задание 1.4/PROGRAM.cpp
@@ -14,19 +14,19 @@ int main()
 
     setlocale(LC_ALL, "rus");
 
- /*1. 
-    cout<<"Ââåäèòå ÷èñëî ïîä êîðíåì è ñòåïåíü êîðíÿ:\n";
+ 1.
+    cout<<"Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã·Ã¨Ã±Ã«Ã® Ã¯Ã®Ã¤ ÃªÃ®Ã°Ã­Ã¥Ã¬ Ã¨ Ã±Ã²Ã¥Ã¯Ã¥Ã­Ã¼ ÃªÃ®Ã°Ã­Ã¿:\n";
     int Chislo, Stepen = 6; 
     double Result = 0; 
     cin>>Chislo; 
     Result = VichWithDiff(Chislo, Stepen); 
     cout<<"\nresult = "<<Result; 
     return 0; 
-}*/
+}
 
   
    /*2. 
-    cout<<"Ðàçìåð ìàññèâà -> ";
+    cout<<"ÃÃ Ã§Ã¬Ã¥Ã° Ã¬Ã Ã±Ã±Ã¨Ã¢Ã  -> ";
     cin>>N; 
     int Massiv[N] = {0}; 
     for(int i = 0; i < N; i++){ 
@@ -46,26 +46,26 @@ int main()
     if(Month == "jan" || Month == "dec" || Month == "feb"){ 
         Idinf = 1+rand()%+2; 
         Tempa = TemperatureData(Day, Idinf); 
-        cout<<Day<<"-ãî "<<Month<<" íà óëèöå "<<Tempa<<" ãðàäóñîâ ëþòîãî ìîðîçà. Áóäåøü ñîñóëüêîé!"; 
+        cout<<Day<<"-Ã£Ã® "<<Month<<" Ã­Ã  Ã³Ã«Ã¨Ã¶Ã¥ "<<Tempa<<" Ã£Ã°Ã Ã¤Ã³Ã±Ã®Ã¢ Ã«Ã¾Ã²Ã®Ã£Ã® Ã¬Ã®Ã°Ã®Ã§Ã . ÃÃ³Ã¤Ã¥Ã¸Ã¼ Ã±Ã®Ã±Ã³Ã«Ã¼ÃªÃ®Ã©!"; 
     }
 
     else if(Month == "march" || Month == "april" || Month == "may"){ 
         Idinf = 3+rand()%+5;
         Tempa = TemperatureData(Day, Idinf);
-        cout<<Day<<"-ãî "<<Month<<" íà óëèöå "<<Tempa<<" ãðàäóñîâ ïðîõëàäû. Îäåâàéòåñü òåïëåå!";
+        cout<<Day<<"-Ã£Ã® "<<Month<<" Ã­Ã  Ã³Ã«Ã¨Ã¶Ã¥ "<<Tempa<<" Ã£Ã°Ã Ã¤Ã³Ã±Ã®Ã¢ Ã¯Ã°Ã®ÃµÃ«Ã Ã¤Ã». ÃŽÃ¤Ã¥Ã¢Ã Ã©Ã²Ã¥Ã±Ã¼ Ã²Ã¥Ã¯Ã«Ã¥Ã¥!";
     }
     else if(Month == "juny" || Month == "july" || Month == "aug"){ 
         Idinf = 6+rand()%+8;
         Tempa = TemperatureData(Day, Idinf);
-        cout<<Day<<"-ãî "<<Month<<" íà óëèöå "<<Tempa<<" ãðàäóñîâ æàðû. Áåãîì íà ìîðå!";
+        cout<<Day<<"-Ã£Ã® "<<Month<<" Ã­Ã  Ã³Ã«Ã¨Ã¶Ã¥ "<<Tempa<<" Ã£Ã°Ã Ã¤Ã³Ã±Ã®Ã¢ Ã¦Ã Ã°Ã». ÃÃ¥Ã£Ã®Ã¬ Ã­Ã  Ã¬Ã®Ã°Ã¥!";
     }
     else if(Month == "sep" || Month == "oct" || Month == "nov"){ 
         Idinf = 9+rand()%+11;
         Tempa = TemperatureData(Day, Idinf);
-        cout<<Day<<"-ãî "<<Month<<" íà óëèöå "<<Tempa<<" ãðàäóñîâ õîëîäà. Òû íîñèøü øàïêó óøàíêó? ";
+        cout<<Day<<"-Ã£Ã® "<<Month<<" Ã­Ã  Ã³Ã«Ã¨Ã¶Ã¥ "<<Tempa<<" Ã£Ã°Ã Ã¤Ã³Ã±Ã®Ã¢ ÃµÃ®Ã«Ã®Ã¤Ã . Ã’Ã» Ã­Ã®Ã±Ã¨Ã¸Ã¼ Ã¸Ã Ã¯ÃªÃ³ Ã³Ã¸Ã Ã­ÃªÃ³? ";
     }
     else 
-        cout<<"Íåêîððåêòíûé ââîä!";
+        cout<<"ÃÃ¥ÃªÃ®Ã°Ã°Ã¥ÃªÃ²Ã­Ã»Ã© Ã¢Ã¢Ã®Ã¤!";
     return 0; */
 
 }
